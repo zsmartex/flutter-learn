309517c1a0526e92e9153c946f4e4a4c7a5864a7
shinhagunn <shinhagunn@gmail.com>
2022-10-12 19:13:25 +0700

    transition page

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,29 +1,12 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:app/screens/market.dart';
 
-class HomePage extends StatefulWidget {
-  @override
- _HomePage createState() => _HomePage();
-}
-
-class _HomePage extends State<HomePage> with SingleTickerProviderStateMixin{
-  late AnimationController controller;
-  late Animation colorAnimation;
-  late Animation sizeAnimation;
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
-  void initState() {
-    super.initState();
-    controller =  AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    colorAnimation = ColorTween(begin: Colors.blue, end: Colors.yellow).animate(controller);
-    sizeAnimation = Tween<double>(begin: 100.0, end: 200.0).animate(controller);
-  }
   Widget build(BuildContext context) {
-    return CupertinoPageTransition(
-      linearTransition: true,
-      primaryRouteAnimation: AnimationController(vsync: this, duration: const Duration(seconds: 2)),
-      secondaryRouteAnimation: AnimationController(vsync: this, duration: const Duration(seconds: 2)),
+    return CupertinoPageScaffold(
       child: CupertinoPageScaffold(
         navigationBar: const CupertinoNavigationBar(
           middle: Text('HOME'),
@@ -32,10 +15,19 @@ class _HomePage extends State<HomePage> with SingleTickerProviderStateMixin{
           child: CupertinoButton(
             child: const Text('bam vao day de xem phim heo'),
             onPressed: () {
-              Navigator.of(context, rootNavigator: true).push(CupertinoPageRoute<Widget>(
-                  builder: (BuildContext context) {
-                return const MarketScreen();
-              }));
+              Navigator.of(context, rootNavigator: true).push(
+                PageRouteBuilder(
+                  pageBuilder: (_, __, ___) => const MarketScreen(),
+                  transitionDuration: const Duration(milliseconds: 200),
+                  transitionsBuilder: (_, a, __, c) => SlideTransition(
+                    position: Tween(
+                        begin: Offset(1.0, 0.0),
+                        end: Offset(0.0, 0.0))
+                    .animate(a),
+                    child: c,
+                  ),
+                )
+              );
             },
           )
         ),
@@ -43,3 +35,8 @@ class _HomePage extends State<HomePage> with SingleTickerProviderStateMixin{
     );
   }
 }
+
+// CupertinoPageRoute<Widget>(
+//                   builder: (BuildContext context) {
+//                 return const MarketScreen();
+//               })
